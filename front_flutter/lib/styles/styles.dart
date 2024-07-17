6ffbe132902fa9347f93bc96c0444e35d8acf48a
Gaspard <95536893+gaspjhnt@users.noreply.github.com>
2024-07-17 16:06:08 +0200

    home page

---
+++ front_flutter/lib/styles/styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF2F70AF);
+const Color secondaryColor = Color(0xFFB9848C);
+const Color tertiaryColor = Color(0xFF806491);
+const Color backgroundColor = Color(0xFF000000);
